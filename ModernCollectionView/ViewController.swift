c819ddcbf82ee152ed66671b20e327f3a693e25b
yeggrrr <yjkim981103@gmail.com>
2024-07-19 14:43:25 +0900

    Initial Commit

---
+++ ModernCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernCollectionView
+//
+//  Created by YJ on 7/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
