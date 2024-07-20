23ddf1e2a7be7faf64bc4cbbfb13d86458f1f070
yeggrrr <yjkim981103@gmail.com>
2024-07-20 16:45:14 +0900

    [Add] TravelTalkVC 추가 & 주석 추가

---
+++ ModernCollectionView/TravelTalkVC/TravelTalkCollectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TravelTalkCollectionViewController.swift
+//  ModernCollectionView
+//
+//  Created by YJ on 7/20/24.
+//
+
+import UIKit
+
+class TravelTalkCollectionViewController: UIViewController {
+    // MARK: UI
+    
+    // MARK: Properties
+    
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: Functions
+}
