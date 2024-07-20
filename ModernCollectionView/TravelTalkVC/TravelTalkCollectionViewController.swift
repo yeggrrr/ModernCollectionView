4e209972616460f967ef9adbc5c95363a01a9e40
yeggrrr <yjkim981103@gmail.com>
2024-07-20 18:31:18 +0900

    [Feat] TravelTalkVC - UI 완성 (imagecornerRadius 오류 수정 예정)

--- ModernCollectionView/TravelTalkVC/TravelTalkCollectionViewController.swift
+++ ModernCollectionView/TravelTalkVC/TravelTalkCollectionViewController.swift
@@ -8,17 +8,41 @@
 import UIKit
 import SnapKit
 
+struct ChattingRoom: Hashable, Identifiable {
+    let id = UUID()
+    let profileImage: String
+    let userName: String
+    let currentChat: String
+}
+
 class TravelTalkCollectionViewController: UIViewController {
+    // MARK: Enum
+    enum Section: CaseIterable {
+        case user
+    }
+    
     // MARK: UI
     lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout())
     
     // MARK: Properties
+    let chattingList = [
+        ChattingRoom(profileImage: "Hue", userName: "Hue", currentChat: "왜요? 요즘 코딩이 대세인데"),
+        ChattingRoom(profileImage: "Jack", userName: "Jack", currentChat: "깃허브는 푸시하셨나여?"),
+        ChattingRoom(profileImage: "Bran", userName: "Bran", currentChat: "과제 화이팅!"),
+        ChattingRoom(profileImage: "Den", userName: "Den", currentChat: "벌써 퇴근하세여?ㅎㅎㅎㅎㅎ"),
+        ChattingRoom(profileImage: "내옆자리의앞자리에개발잘하는친구", userName: "내옆자리의앞자리에개발잘하는친구", currentChat: "내일 모닝콜 해주실분~"),
+        ChattingRoom(profileImage: "심심이", userName: "심심이", currentChat: "아닛 주말과제라닛")
+    ]
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, ChattingRoom>!
     
     // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configure()
+        configureDataSource()
+        updateSnapShot()
     }
     
     // MARK: Functions
@@ -47,12 +71,48 @@ class TravelTalkCollectionViewController: UIViewController {
     
     func collectionViewLayout() -> UICollectionViewLayout {
         var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
-        configuration.backgroundColor = .white
+        configuration.backgroundColor = .systemBackground
         configuration.showsSeparators = true
         
         let layout = UICollectionViewCompositionalLayout.list(using: configuration)
         return layout
     }
     
+    func configureDataSource() {
+        var registration: UICollectionView.CellRegistration<UICollectionViewListCell, ChattingRoom>!
+        registration = UICollectionView.CellRegistration { cell, indexPath, itemIdentifier in
+            var content = UIListContentConfiguration.cell()
+            content.text = itemIdentifier.userName
+            content.textProperties.font = .systemFont(ofSize: 15, weight: .semibold)
+            content.textProperties.color = .label
+            
+            content.secondaryText = itemIdentifier.currentChat
+            content.secondaryTextProperties.font = .systemFont(ofSize: 13, weight: .regular)
+            content.secondaryTextProperties.color = .lightGray
+            
+            content.image = UIImage(named: itemIdentifier.profileImage)
+            content.imageProperties.reservedLayoutSize = CGSize(width: 40, height: 40)
+            content.imageProperties.maximumSize = CGSize(width: 50, height: 50)
+            content.imageProperties.cornerRadius = 25
+            content.imageToTextPadding = 20
+            
+            cell.contentConfiguration = content
+            
+            var bgConfig = UIBackgroundConfiguration.listPlainCell()
+            bgConfig.backgroundColor = .systemBackground
+            cell.backgroundConfiguration = bgConfig
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: itemIdentifier)
+            return cell
+        })
+    }
     
+    func updateSnapShot() {
+        var snapShot = NSDiffableDataSourceSnapshot<Section, ChattingRoom>()
+        snapShot.appendSections(Section.allCases)
+        snapShot.appendItems(chattingList, toSection: Section.user)
+        dataSource.apply(snapShot)
+    }
 }
